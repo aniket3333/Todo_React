5a431956ec65176657408be74d8360f3132f7e0e
aniket3333 <you@example.comanikethiware3@gmail.com>
2023-08-17 16:57:26 +0530

    Add mobile classes and Delete Dunctionality

--- src/Components/Show.js
+++ src/Components/Show.js
@@ -1,17 +1,64 @@
-import React from 'react'
-
-const Show = ({tas}) => {
+import React from "react";
+import { MdDelete } from "react-icons/md";
+import { RxUpdate } from "react-icons/rx";
+import { Button } from "react-bootstrap";
+const Show = ({ tas, onDeleteClick, AllDelete }) => {
   return (
     <>
-   <ul>
-   {
-        tas.map((item,index)=>(
-            <li key={index} className='p-1 text-md text-success'>{item}</li>
-        ))
-      }
-   </ul>
+      <h4 className="text-muted p-2">
+        List of Added Task
+        {tas.length > 1 && (
+          <span className="p-2">
+            <span style={{ color: "red" }}> All </span>
+            <Button
+              className="p-1"
+              style={{ color: "red" }}
+              onClick={() => AllDelete()}
+            >
+             
+              <MdDelete />
+            </Button>
+          </span>
+        )}
+      </h4>
+      <div className="border mt-2 p-3">
+        <ol>
+          {tas.map((item, index) => (
+            <li key={index}>
+              <div className="row p-2">
+                <div className="col-4">
+                  <span
+                    className="text-capitalize"
+                    style={{
+                      fontSize: "20px",
+                      fontFamily: "serif",
+                      color: "black",
+                    }}
+                  >
+                    {item}
+                  </span>
+                </div>
+                <div className="col-2">
+                  <Button
+                    className="p-1"
+                    style={{ color: "red" }}
+                    onClick={() => onDeleteClick(index)}
+                  >
+                    <MdDelete />
+                  </Button>
+                </div>
+                <div className="col-2">
+                  <Button className="p-1" style={{ color: "white" }}>
+                    <RxUpdate />
+                  </Button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Show
+export default Show;
